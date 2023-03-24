4670a860df96242ce7b23eb8b6edaaf58e6f376b
omerav4 <101065282+omerav4@users.noreply.github.com>
2023-03-24 22:56:56 +0300

    q3d

--- ex1/create.sql
+++ ex1/create.sql
@@ -17,3 +17,41 @@ create table enrollment(
 	year varchar, 
 	students5_estimated varchar
 );
+
+create table Country(
+    countrycode varchar primary key,
+    country varchar,
+    region varchar,
+    incomegroup varchar
+);
+
+create table University(
+    iau_id1 varchar primary key,
+	eng_name varchar,
+	orig_name varchar,
+	foundedyr varchar,
+	yrclosed varchar,
+	private01 varchar
+);
+
+create table Enrollment_stats(
+    year varchar primary key,
+	students5_estimated varchar,
+    phd_granting varchar,
+	divisions varchar,
+	specialized varchar
+);
+
+create table Located_at(
+    iau_id1 varchar primary key,
+	country_code varchar,
+    latitude varchar,
+	longitude varchar
+);
+
+create table University_annual_stats(
+    iau_id1 varchar primary key,
+	year varchar,
+	primary key (iau_id1, year)
+);
+
--- ex1/drop.sql
+++ ex1/drop.sql
@@ -1,1 +1,6 @@
 drop table enrollment;
+drop table Country;
+drop table University;
+drop table Enrollment_stats;
+drop table Located_at;
+drop table University_annual_stats;
