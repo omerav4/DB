3452d0b115aadc01f732b6e200110db5a14fb67d
omerav4 <101065282+omerav4@users.noreply.github.com>
2023-03-29 10:02:05 +0300

    change create file

--- ex1/create.sql
+++ ex1/create.sql
@@ -1,49 +1,32 @@
-create table enrollment(
-	country varchar, 
-	countrycode varchar, 
-	region varchar, 
-	incomegroup varchar, 
-	iau_id1 varchar, 
-	eng_name varchar, 
-	orig_name varchar, 
-	foundedyr varchar, 
-	yrclosed varchar, 
-	private01 varchar, 
-	latitude varchar, 
-	longitude varchar, 
-	phd_granting varchar, 
-	divisions varchar, 
-	specialized varchar, 
-	year varchar, 
-	students5_estimated varchar
-);
-
 create table Country(
-    countrycode varchar primary key,
-    country varchar,
-    region varchar,
-    incomegroup varchar
+    countrycode varchar(3) primary key,
+    country varchar NOT NULL,
+    region varchar NOT NULL,
+    incomegroup varchar NOT NULL check(incomegroup IN ('High income', 'Low income', 'Lower middle income', 'Upper middle income'))
 );
 
-create table University(
+create table Universities(
     iau_id1 varchar primary key,
-	eng_name varchar,
-	orig_name varchar,
-	foundedyr varchar,
-	yrclosed varchar,
-	private01 varchar,
-    phd_granting varchar,
-	divisions varchar,
-	specialized varchar,
-    country_code varchar,
-    latitude varchar,
-	longitude varchar
+    eng_name varchar NOT NULL,
+    orig_name varchar NOT NULL,
+    foundedyr integer NOT NULL,
+    yrclosed integer,
+    check(NULL OR foundedyr <= yrclosed),
+    private01 integer check(private01 = 0 OR private01 = 1),
+    phd_granting integer NOT NULL check(phd_granting = 0 OR phd_granting = 1),
+    divisions integer,
+    specialized integer NOT NULL check(specialized = 0 OR specialized = 1),
+    countrycode varchar(3) NOT NULL
+    references Country(countrycode),
+    latitude float,
+    longitude float
 );
 
-create table Enrollment_stats(
+create table enrollment_stats(
     iau_id1 varchar,
-    year varchar,
-	students5_estimated varchar,
-	primary key (iau_id1, year)
+    year integer,
+    students5_estimated integer,
+    primary key(iau_id1, year),
+    foreign key(iau_id1)
+    references Universities(iau_id1)
 );
-
