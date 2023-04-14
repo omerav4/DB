7c66c34f3b67e278b66e2760c7187a2b83ab0b1c
omerav4 <101065282+omerav4@users.noreply.github.com>
2023-04-14 20:43:38 +0300

    changes

--- ex2/q4.sql
+++ ex2/q4.sql
@@ -2,7 +2,7 @@ select members1.name
 from members members1 join memberInKnesset memberInKnesset1 on members1.uid = memberInKnesset1.uid
 where members1.occupation <> 'politician' and members1.gender = 'female' and memberInKnesset1.number = 23
 and exists
-(select members.name
+(select members2.name
 from members members2 join memberInKnesset memberInKnesset2 on members2.uid = memberInKnesset2.uid
 where members2.occupation <> 'politician' and members2.gender = 'female' and memberInKnesset2.number = 24)
 order by members1.name
