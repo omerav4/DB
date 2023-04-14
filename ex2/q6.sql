6dc9e0509f909f28ddcf3948fbd9a2b0765ae319
omerav4 <101065282+omerav4@users.noreply.github.com>
2023-04-14 20:52:03 +0300

    changes

--- ex2/q6.sql
+++ ex2/q6.sql
@@ -7,5 +7,5 @@ join knessets on knessets.number = memberInKnesset.number
 where memberInKnesset.party <> 'Mapai' and knessets.number in
 (select memberInKnesset.number from members join memberInKnesset on members.uid = memberInKnesset.uid
 where members.name = 'David Ben Gurion' and memberInKnesset.party = 'Mapai')
-order by members.name
+--order by members.name
 
