fe7445b976126252d07af69c623d8cb502e9f1eb
omerav4 <101065282+omerav4@users.noreply.github.com>
2023-04-15 15:25:59 +0300

    changes

--- ex2/q7.sql
+++ ex2/q7.sql
@@ -6,7 +6,7 @@ where exists
               from members
                        join memberInKnesset memberInKnesset2 on members.uid = memberInKnesset2.uid
               where memberInKnesset1.number = memberInKnesset2.number
-                and members.age = (select max(members.age)
+                and members.birthYear = (select min(members.birthYear)
                                    from members
                                             join memberInKnesset on members.uid = memberInKnesset.uid
                                    where memberInKnesset.number = memberInKnesset1.number)
