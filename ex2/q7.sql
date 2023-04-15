f09657153114fd652f200216523736d5f93ca505
omerav4 <101065282+omerav4@users.noreply.github.com>
2023-04-15 17:49:21 +0300

    changes

--- ex2/q7.sql
+++ ex2/q7.sql
@@ -1,12 +1,13 @@
-select memberInKnesset1.number, members.name
-from members join memberInKnesset memberInKnesset1 on members.uid = memberInKnesset1.uid
+select distinct memberInKnesset1.number, members1.name
+from members members1 join memberInKnesset memberInKnesset1 on members.uid = memberInKnesset1.uid
 where exists
           (
-              select memberInKnesset2.number, members.name
-              from members join memberInKnesset memberInKnesset2 on members.uid = memberInKnesset2.uid
-              where memberInKnesset1.number = memberInKnesset2.number
-                and members.birthYear = (select min(members.birthYear)
-                                   from members join memberInKnesset on members.uid = memberInKnesset.uid
-                                   where memberInKnesset.number = memberInKnesset1.number)
+              select distinct memberInKnesset2.number, members2.name
+              from members members2 join memberInKnesset memberInKnesset2 on members2.uid = memberInKnesset2.uid
+              where memberInKnesset2.number = memberInKnesset1.number
+                and members2.birthYear = (select min(members3.birthYear)
+                                   from members members3 join memberInKnesset memberInKnesset3
+                                       on members3.uid = memberInKnesset3.uid
+                                   where memberInKnesset3.number = memberInKnesset2.number)
           )
 order by memberInKnesset1.number, members.name
