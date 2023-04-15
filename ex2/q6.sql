528ed591665d97905615228b81da821dadff423d
omerav4 <101065282+omerav4@users.noreply.github.com>
2023-04-15 18:05:26 +0300

    changes

--- ex2/q6.sql
+++ ex2/q6.sql
@@ -10,14 +10,24 @@
 -- -- --order by members.name
 
 
-select distinct members1.name
-from members members1 join memberInKnesset on members1.uid = memberInKnesset.uid
-where count(
-select memberInKnesset.number from members1 join memberInKnesset on members1.uid = memberInKnesset.uid
-where members1.name = 'David Ben Gurion' and memberInKnesset.party = 'Mapai'
-except
-select memberInKnesset.number from members members2 join memberInKnesset on members3.uid = memberInKnesset.uid
-where members1.name = members2.name and memberInKnesset.party = 'Mapai') <= 0
---order by members1.name
+-- select distinct members1.name
+-- from members members1 join memberInKnesset on members1.uid = memberInKnesset.uid
+-- where count(
+-- select memberInKnesset.number from members1 join memberInKnesset on members1.uid = memberInKnesset.uid
+-- where members1.name = 'David Ben Gurion' and memberInKnesset.party = 'Mapai'
+-- except
+-- select memberInKnesset.number from members members2 join memberInKnesset on members3.uid = memberInKnesset.uid
+-- where members1.name = members2.name and memberInKnesset.party = 'Mapai') <= 0
+-- --order by members1.name
 
-select
+SELECT DISTINCT members1.name
+FROM members members1
+JOIN memberInKnesset mk1 ON members1.uid = mk1.uid
+WHERE mk1.party = 'Mapai'
+AND mk1.number IN (
+  SELECT mk2.number
+  FROM members members2
+  JOIN memberInKnesset mk2 ON members2.uid = mk2.uid
+  WHERE members2.name = 'David Ben Gurion'
+  AND mk2.party = 'Mapai'
+order by members1.name
