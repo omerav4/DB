b90f0467753aee21e6903090033dcd5fcf6585a2
omerav4 <101065282+omerav4@users.noreply.github.com>
2023-04-17 18:04:36 +0300

    changes

--- ex2/q5.sql
+++ ex2/q5.sql
@@ -1,6 +1,10 @@
-select members.name
-from members join memberInKnesset on members.uid = memberInKnesset.uid
-where members.birthPlace = 'Jerusalem'
-group by members.name
-having count(memberInKnesset.number) = 1
+select members1.name
+from members members1 join memberInKnesset memberInKnesset1 on members1.uid = memberInKnesset1.uid
+where members1.birthPlace = 'Jerusalem' and not exists
+(
+    select memberInKnesset2.uid
+    from memberInKnesset memberInKnesset2
+    where memberInKnesset2.uid = members1.uid
+    and memberInKnesset2.number <> memberInKnesset1.number
+    )
 order by members.name
--- ex2/q6.sql
+++ ex2/q6.sql
@@ -1,33 +1,10 @@
--- select distinct members.name
--- -- from members
--- -- except
--- -- select distinct members.name
--- -- from members join memberInKnesset on members.uid = memberInKnesset.uid
--- -- join knessets on knessets.number = memberInKnesset.number
--- -- where memberInKnesset.party <> 'Mapai' and knessets.number in
--- -- (select memberInKnesset.number from members join memberInKnesset on members.uid = memberInKnesset.uid
--- -- where members.name = 'David Ben Gurion' and memberInKnesset.party = 'Mapai')
--- -- --order by members.name
-
-
--- select distinct members1.name
--- from members members1 join memberInKnesset on members1.uid = memberInKnesset.uid
--- where count(
--- select memberInKnesset.number from members1 join memberInKnesset on members1.uid = memberInKnesset.uid
--- where members1.name = 'David Ben Gurion' and memberInKnesset.party = 'Mapai'
--- except
--- select memberInKnesset.number from members members2 join memberInKnesset on members3.uid = memberInKnesset.uid
--- where members1.name = members2.name and memberInKnesset.party = 'Mapai') <= 0
--- --order by members1.name
-
-SELECT DISTINCT members1.name
-FROM members members1
-JOIN memberInKnesset mk1 ON members1.uid = mk1.uid
-WHERE mk1.party = 'Mapai'
-AND mk1.number IN (
-  SELECT mk2.number
-  FROM members members2
-  JOIN memberInKnesset mk2 ON members2.uid = mk2.uid
-  WHERE members2.name = 'David Ben-Gurion'
-  AND mk2.party = 'Mapai')
-order by members1.name
+select distinct members1.name
+from members members1 join memberInKnesset memberInKnesset1 ON members1.uid = memberInKnesset1.uid
+where exists(
+    select helper1.number from (select memberInKnesset2.number
+  from members members2 join memberInKnesset memberInKnesset2 ON members2.uid = memberInKnesset2.uid
+  where members2.name = 'David Ben-Gurion' and memberInKnesset2.party = 'Mapai') helper1
+    where helper1.number in (select memberInKnesset3.number
+  from members members3 join memberInKnesset memberInKnesset3 ON members3.uid = memberInKnesset3.uid
+  where members3.name = members1.name and memberInKnesset3.party = 'Mapai'))
+)
