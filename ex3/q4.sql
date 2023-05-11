43fd09c2321272352d024d9f11f1549098e32d4b
omerav4 <101065282+omerav4@users.noreply.github.com>
2023-05-11 20:57:55 +0300

    ex3

--- ex3/q4.sql
+++ ex3/q4.sql
@@ -5,7 +5,7 @@ from memberInKnesset memberInKnesset1
 where count(memberInKnesset1.uid) = (select max(count(memberInKnesset1.uid))
 from memberInKnesset memberInKnesset3
 where memberInKnesset3.number = memberInKnesset2.number
-group by party))
+group by party)
 group by number, party
 order by number, party;
 
