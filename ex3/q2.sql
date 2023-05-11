e8ef59ddb3cfb63e24be7406d8c09ba9ae083b60
omerav4 <101065282+omerav4@users.noreply.github.com>
2023-05-11 20:41:02 +0300

    ex3

--- ex3/q2.sql
+++ ex3/q2.sql
@@ -1,3 +1,12 @@
-select memberInKnesset.number, avg(2023-members.birthYear) as avgAge
-from memberInKnesset left join members on memberInKnesset.uid = members.uid
+with membersAge as
+(select uid, 2023-birthYear as age
+from members)
+select memberInKnesset.number, avg(age) as avgAge
+from memberInKnesset left join membersAge on memberInKnesset.uid = membersAge.uid
 group by memberInKnesset.number
+order by memberInKnesset.number
+
+-- select memberInKnesset.number, 2023-members.birthYear) as avgAge
+-- from memberInKnesset left join members on memberInKnesset.uid = members.uid
+-- group by memberInKnesset.number
+-- order by memberInKnesset.number
